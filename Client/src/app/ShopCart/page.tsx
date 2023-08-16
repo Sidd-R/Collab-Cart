72b2218bebe2632b394f6c1032dd09b2ab62700b
KomalT2003 <komal.tarachandani@spit.ac.in>
2023-08-16 13:44:25 +0530

    contributors with people list

--- Client/src/app/ShopCart/page.tsx
+++ Client/src/app/ShopCart/page.tsx
@@ -247,6 +247,29 @@ const policies = [
     description: 'We’ve pledged 1% of sales to the preservation and restoration of the natural environment.',
   },
 ]
+
+const people = [
+  {
+    name: 'Leonard Krasner',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Floyd Miles',
+    imageUrl:
+      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Emily Selman',
+    imageUrl:
+      'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristin Watson',
+    imageUrl:
+      'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
 const footerNavigation = {
   products: [
     { name: 'Bags', href: '#' },
@@ -402,6 +425,7 @@ export default function ShopCart() {
                         </p>
                         <button 
                         className="my-1 text-blue-500 text-xs w-1/6 px-3 bg-yellow-300 border border-transparent rounded-md shadow-sm  text-base text-white hover:bg-yellow-400  "
+                        onClick={()=>{setContriModal(true)}}
                         >
                           Contributors
                         </button>
@@ -410,7 +434,38 @@ export default function ShopCart() {
                   ))}
                 </ul>
               </section>
-
+              {contriModal? (
+                <>
+          <div className="flex overflow-x-hidden overflow-y-auto fixed inset-0  ">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className='flex justify-between mx-5 my-2'>
+              <span>Contributors</span>
+              <button onClick={()=>setContriModal(false)}>X</button>
+              </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                <div className="flow-root mt-6">
+        <ul role="list" className="-my-5 divide-y divide-gray-200">
+          {people.map((person) => (
+            <li key={person.handle} className="py-4">
+              <div className="flex items-center space-x-4">
+                <div className="flex-shrink-0">
+                  <img className="h-8 w-8 rounded-full" src={person.imageUrl} alt="" />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
               {/* Order summary */}
               <section aria-labelledby="summary-heading" className="mt-10 sm:ml-6 sm:pl-6">
                 <div className="bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8">
