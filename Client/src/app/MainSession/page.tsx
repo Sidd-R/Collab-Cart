3d815f2859158337c101f99f4505800fa9d6acb8
Sidd-R <siddhant.rao@spit.ac.in>
2023-08-17 16:38:40 +0530

    build error resolved

--- Client/src/app/MainSession/page.tsx
+++ Client/src/app/MainSession/page.tsx
@@ -135,20 +135,7 @@ export default function Example() {
                                         ))}
                                     </nav>
                                 </div>
-                                <div className="flex-shrink-0 flex bg-gray-100 p-4">
-                                    <a href="#" className="flex-shrink-0 group block">
-                                        <div className="flex items-center">
-                                            <div>
-                                                <img
-                                                    className="inline-block h-10 w-10 rounded-full"
-                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                    alt=""
-                                                />
-                                            </div>
-
-                                        </div>
-                                    </a>
-                                </div>
+                                
                             </div>
                         </Transition.Child>
                         <div className="flex-shrink-0 w-14">{/* Force sidebar to shrink to fit close icon */}</div>
